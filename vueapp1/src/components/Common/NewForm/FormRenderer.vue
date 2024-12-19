03ed10fc059a357f7e46296f6e0bc45225507513
Sakthivel Munusamy <106135104+Sakthi020@users.noreply.github.com>
2024-12-19 13:04:21 +0530

    code added

---
+++ vueapp1/src/components/Common/NewForm/FormRenderer.vue
@@ -0,0 +1,382 @@
+<template>
+
+	<div class="row">
+
+		<!-- Use a combination of formSection.id and randomKey as the unique key to ensure reactivity and avoid key collisions -->
+		<div v-for="(formSection, index) in formSections" :key="formSection.id+''+randomKey" :id="formSection.id" class="col-md-12">
+
+			<FormSectionRenderer :formSection="formSection"
+			                     :formId="formId"
+			                     :formUniqueKey="formUniqueKey"
+			                     :panel="panel"
+			                     :scenario="scenario"
+			                     :formType="category"
+			                     :btn-style="btnStyle"
+			                     :input-style="inputStyle"
+			                     :isInlineForm="isInlineForm"
+			                     :sizeModules="sizeModules"
+			                     :apiKey="apiKey"
+			                     :selectDependencyId="selectDependencyId"
+			                     :selectDependencyKey="selectDependencyKey"
+			                     :recur_mode="recur_mode"
+			                     :callingFrom="callingFrom"
+			>
+
+				<!-- Slot for rendering 'ticketedit' content in specific conditions -->
+				<template v-slot:button v-if="index === 0 && category === 'ticket' && scenario === 'edit' && panel === 'agent'">
+					<div class="card-tools">
+						<slot name="ticketedit"></slot>
+					</div>
+				</template>
+
+			</FormSectionRenderer>
+		</div>
+	</div>
+</template>
+
+<script>
+
+import FormFieldRenderer from "./FormFieldRenderer.vue";
+import Loader from "../../MiniComponent/Loader.vue";
+import {errorHandler} from "../../../helpers/responseHandler";
+import {mapActions, mapGetters, mapState} from "vuex";
+import FormSectionRenderer from "./FormSectionRenderer.vue";
+import _ from "lodash-core";
+import {boolean, MULTIPLE_PROPERTY_HELPER} from "../../../helpers/extraLogics";
+
+export default {
+
+	name: 'form-renderer',
+
+	/**
+	 * Props passed to the component:
+	 * - recur_mode: Specifies the mode for recurring events. Default is an empty string.
+	 * - template_mode: Specifies the mode for templates. Default is an empty string.
+	 * - fetchFormFieldApi: The API endpoint to fetch form field data. Required.
+	 * - formUniqueKey: The unique key for identifying the form instance. Required.
+	 * - category: The category of the form. Required.
+	 * - scenario: The scenario in which the form is being used. Required.
+	 * - panel: The panel where the form is displayed. Required.
+	 * - isChildForm: Boolean indicating if the form is a child form. Default is false.
+	 * - disableEmail: Boolean indicating if email fields are disabled. Default is false.
+	 * - inputStyle: Custom input styles for the form. Default is an empty object.
+	 * - btnStyle: Custom button styles for the form. Default is an empty object.
+	 * - isInlineForm: Boolean indicating if the form is inline. Default is true.
+	 * - formId: The ID of the form. Default is an empty string.
+	 * - apiKey: The API key for fetching data. Default is an empty string.
+	 * - selectDependencyId: The ID of the select dependency. Default is an empty string.
+	 * - selectDependencyKey: The key of the select dependency. Default is an empty string.
+	 */
+	props: {
+		recur_mode: {type: String, default: ''},
+		template_mode: {type: String, default: ''},
+		fetchFormFieldApi: {type: String, required: true},
+		formUniqueKey: {type: String, required: true},
+		category: {type: String, required: true},
+		scenario: {type: String, required: true},
+		panel: {type: String, required: true},
+		isChildForm: {type: Boolean, default: false},
+		disableEmail: {type: Boolean, default: false},
+		inputStyle: { type: Object, default: () => {}},
+		btnStyle: { type: Object, default: () => {}},
+		isInlineForm: { type: Boolean, default: true },
+		formId: { type: String, default: '' },
+		apiKey : { type : String, default : '' },
+		selectDependencyId : { type : [String, Number], default : '' },
+		selectDependencyKey : { type : String, default : '' },
+		callingFrom: { type : String, default : ''}
+	},
+
+	/**
+	 * Data properties for the component:
+	 * - formSections: An array to store form sections. Default is an empty array.
+	 * - sizeModules: Specifies the size modules for the form. Default is an empty string.
+	 */
+	data: () => {
+		return {
+			formSections: [],
+			sizeModules: '',
+			triggerValidation: false,
+			reloadForm: false,
+			randomKey: 0
+		}
+	},
+
+	computed: {
+
+		/**
+		 * Maps the 'setAttachmentsData' action.
+		 */
+		...mapActions(['setAttachmentsData']),
+
+		/**
+		 * Maps the 'getCategory' getter.
+		 */
+		...mapGetters(['getCategory']),
+
+		/**
+		 * Computes and returns an object containing API parameters including category, scenario, panel, and api_key.
+		 */
+		getApiParams() {
+
+			return {category: this.category, scenario: this.scenario, panel: this.panel, api_key : this.apiKey}
+		}
+	},
+
+	/**
+	 * Executes logic upon component creation.
+	 * - Retrieves form sections based on the category.
+	 * - Listens for 'updateTicketCategory' event to update form sections.
+	 * - Listens for 'modelFormFieldFetched' event to update form sections based on fetched model form fields.
+	 */
+	created() {
+
+		this.getFormSections(this.getCategory);
+
+		const urlParams = new URLSearchParams(this.fetchFormFieldApi.split('?')[1]);
+		if (urlParams.get('checkTicketStatus') === 'true') {
+			this.triggerValidation = true;
+		}
+
+		// Listen for the 'editTicketChangeStatus' event and set reloadForm to true when triggered
+		window.emitter.on('editTicketChangeStatus', ()=> this.reloadForm = true);
+
+		window.emitter.on('modelFormFieldFetched', (value)=>{
+
+			this.formSections = this.formSections.filter(item => {
+
+				if ('node_identifier' in item) {
+
+					return item.node_identifier !== value.identifier;
+				}
+
+				return true;
+			});
+
+			this.formSections =  [...this.formSections, ...value.nodes];
+		});
+
+		window.emitter.on('disableSpecificFormField', (value)=>{
+
+			this.disableAt(this.formSections, value.identifier, value.disabled, value.setValue);
+		});
+	},
+
+	/**
+	 * Watches for changes in the 'getCategory' computed property.
+	 * - Calls 'getFormSections' method with the new category value.
+	 */
+	watch: {
+
+		getCategory(newCategory) {
+
+			this.getFormSections(newCategory);
+		},
+
+		reloadForm(newValue, oldValue) {
+			// If reloadForm is set to true, fetch the form sections again
+			if(newValue === true) {
+
+				this.getFormSections(this.getCategory);
+
+				// Trigger validation after reloading the form
+				this.triggerValidation = true;
+
+				// Reset reloadForm to false after reloading
+				this.reloadForm = false;
+			}
+		}
+	},
+
+	methods: {
+
+		/**
+		 * Fetches form sections based on the provided category.
+		 * @param {String} category - The category for which form sections need to be fetched.
+		 */
+		getFormSections(category = null) {
+
+			let params = this.getApiParams;
+
+			if(category) {
+
+				params.category = category;
+
+				this.formSections=[]
+			}
+
+			if (this.isChildForm) {
+
+				this.$store.dispatch('startChildFormLoader');
+
+			} else {
+
+				this.$store.dispatch('startLoader', 'getFormSections');
+			}
+
+			axios.get(this.fetchFormFieldApi, {params:params}).then(response => {
+
+				this.formSections = response.data.data.form_sections;
+
+				if(this.triggerValidation) {
+					// Update randomKey with the current timestamp to ensure reactivity
+					this.randomKey = Date.now();
+				}
+
+				this.sizeModules = response.data.data;
+
+				let attachmentsResponse = response.data.data.attachments;
+
+				this.$store.dispatch('setAttachmentsData', attachmentsResponse);
+
+				if(this.scenario !== 'recur' && this.scenario !== 'template'){
+
+					const formFields = this.formSections[0].form_fields;
+					const fieldToFocus = formFields.first?.[0] || formFields.second?.[0];
+
+					if (fieldToFocus) {
+						fieldToFocus['auto_focus'] = true;
+					}
+				}
+
+				window.emitter.emit('storeCategoryId', response.data.data.form_category_id ? response.data.data.form_category_id : response.data.data.id)
+
+				window.emitter.emit('updateOrgUserFormOrg', response.data.data.selectedorg ? response.data.data.selectedorg : '')
+
+				if((this.scenario === 'recur' && this.recur_mode === 'edit' && response.data.data.form) || (this.scenario === 'template' && this.template_mode === 'edit' && response.data.data.form)) {
+
+					window.emitter.emit('updateSelectedForm',response.data.data.form);
+				}
+
+				if (this.disableEmail) {
+
+					this.disableAt(this.formSections, "email");
+				}
+
+				this.$store.dispatch('updateSubmitApiEndpoint', {
+					formUniqueKey: this.formUniqueKey,
+					submitApiEndpoint: response.data.data.submit_endpoint
+				});
+
+				if(this.callingFrom !== 'popup') {
+
+					const recurData = response.data.data.recur ?? { id: null, name: '', interval: '', delivery_on: '', start_date: '', end_date: '', execution_time: ''} ;
+
+					if (recurData) {
+
+						this.$store.dispatch('setEditorAttachments', response.data.data.attachments ?? [])
+
+						for (const [key, value] of Object.entries(recurData)) {
+
+							this.$store.dispatch('updateRecurProperties', {key: key, value: value});
+						}
+					}
+
+					const templateData = response.data.data.template ?? { id: null, name: '' };
+
+					if (templateData) {
+
+						for (const [key, value] of Object.entries(templateData)) {
+
+							this.$store.dispatch('updateTemplateProperties', {key: key, value: value});
+						}
+					}
+
+					this.$store.dispatch('setBatchTicketMode', response.data.data.batch_ticket_status);
+				}
+			}).catch(err => {
+
+				errorHandler(err, 'form-renderer');
+
+			}).finally(() => {
+
+				if (this.isChildForm) {
+
+					this.$store.dispatch('stopChildFormLoader');
+
+				} else {
+
+					this.$store.dispatch('stopLoader', 'getFormSections');
+				}
+
+				if(this.triggerValidation) {
+					// Emit the 'validateEditForm' event to trigger form validation
+					window.emitter.emit('validateEditForm');
+					// Reset triggerValidation to false after emitting the event
+					this.triggerValidation = false;
+				}
+			})
+		},
+
+		/**
+		 * Disables form fields with the specified form identifier in the provided array.
+		 * @param {Array} array - The array of form fields.
+		 * @param {String} form_identifier - The form identifier to be disabled.
+		 */
+		disableAt(array, form_identifier, disabledVal = true, setValue = false){
+
+			let idx = _.findIndex(array, (element) => element.key === form_identifier);
+
+			if(idx !== -1) {
+
+				array[idx].disabled = disabledVal;
+
+				if(!setValue) {
+
+					window.emitter.emit('updateSpecificFormField',{name : form_identifier, value: array[idx].value });
+
+				} else {
+
+					let apiUrl = MULTIPLE_PROPERTY_HELPER.convertStringOfPropertiesToObject(array[idx].api_info).url;
+
+					axios.get(apiUrl).then(response => {
+
+						let data = response.data.data.data;
+
+						let value = data.find(item => item.name === 'Draft');
+
+						window.emitter.emit('updateSpecificFormField',{name : form_identifier, value: value });
+
+					}).catch(err => {
+
+						errorHandler(err, 'form-renderer');
+					})
+				}
+
+			} else {
+
+				for (let i = 0; i < array.length; i++) {
+
+					if (array[i].form_fields && typeof array[i].form_fields === 'object') {
+
+						Object.keys(array[i].form_fields).forEach(key => {
+
+							this.disableAt(array[i].form_fields[key],form_identifier, disabledVal, setValue)
+						});
+
+					} else {
+
+						if(boolean(array[i].options)) {
+
+							array[i].options.forEach(element => {
+
+								this.disableAt(element.nodes,form_identifier, disabledVal, setValue)
+							})
+						}
+					}
+				}
+			}
+		}
+	},
+
+	components: {
+		FormSectionRenderer,
+		FormFieldRenderer,
+		Loader
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vueapp1/src/components/Common/tinyMCE/TinyMCEWithValidation.vue
@@ -0,0 +1,364 @@
+<template>
+	<form-field-template :label="label" :labelStyle="labelStyle" :name="name" :classname="classname" :hint="hint" :required="required" :isInlineForm="isInlineForm" :tipRule="tipRule"
+	                     :labelLength="labelLength">
+
+<!--		<file-manager-container v-if="mediaOption && panel != 'client'"-->
+<!--		                        v-on:filesChosen="getAttachmentInfo"-->
+<!--		/>-->
+
+		<ValidationProvider :name="name" :rules="rules" v-model="editorValue">
+			<template v-slot="{ field, errorMessage, meta, classes }">
+
+				<transition name="fade">
+					<div v-if="showMsg" class="full-screen-msg-block">
+						<span class="full-screen-msg">Press <strong>Esc</strong> to exit Full Screen mode</span>
+					</div>
+				</transition>
+
+				<editor :init="options" v-model="editorValue" @onChange="onChange(editorValue, name)" :name="name" :class="errorMessage ? 'field-danger' : ''" />
+
+				<span v-if="errorMessage" class="error-block is-danger">{{errorMessage}}</span>
+
+			</template>
+		</ValidationProvider>
+
+		<template v-if="loading">
+			<custom-loader duration="4000" />
+		</template>
+
+		<template v-for="(attachment,index) in attachmentsArr">
+
+			<div id='hidden-attach' contenteditable='false' v-if="attachment.disposition !== 'inline'">
+
+				{{attachment.name}}({{attachment.size ? attachment.size : attachment.file_size}} bytes)
+
+				<i class='fa fa-times close-icon' aria-hidden='true' @click='removeAttachment(index)'></i>
+			</div>
+		</template>
+
+	</form-field-template>
+</template>
+
+<script>
+
+import Editor from '@tinymce/tinymce-vue';
+import { plugins, editorProps } from "./tinyMceDefaults";
+import FormFieldTemplate from "../../MiniComponent/FormField/FormFieldTemplate.vue";
+// import FileManagerContainer from "../../Agent/Filemanager/FileManagerContainer.vue";
+import  {mapGetters,mapActions,mapMutations,mapState} from "vuex";
+
+
+export default {
+
+	name: "tiny-editor-with-validation",
+
+	description: 'TinyMCE Editor With Validations',
+
+	props: {
+
+		...editorProps,
+
+		label: { type: String, default: '' },
+
+		name: { type: String, required: false },
+
+		value: { type: String, required: false },
+
+		isInlineForm: { type: Boolean, default: false },
+
+		required: { type: Boolean, default: false },
+
+		rules: { type: String, default: '' },
+
+		hint: { type: String, default: '' },
+
+		/**
+		 * for show labels of the fields
+		 * @type {Object}
+		 */
+		labelStyle:{type:Object},
+
+		/**
+		 * classname of the form field. It can be used to give this component any bootstrap class or a custom class
+		 * whose css will be defined in parent class
+		 * @type {String}
+		 */
+		classname: { type: String, default: "" },
+
+		/**
+		 * The function which will be called as soon as value of the field changes
+		 * It should have two arguments `value` and `name`
+		 *     `value` will be the updated value of the field
+		 *     `name` will be thw name of the state in the parent class
+		 *
+		 * An example function :
+		 *         onChange(value, name){
+		 *             this[name]= selectedValue
+		 *         }
+		 *
+		 * @type {Function}
+		 */
+		onChange: { type: Function, Required: true },
+
+		mediaOption: { type: [Boolean, Number], default: false },
+
+		attachments : { type : Array, default : ()=> []},
+
+		getAttach : { type : Function },
+
+		panel : { type : String, default : '' },
+
+		/**
+		 * For restricting Editor to resolve URLs to relative or absolute URL
+		 * @see https://www.tiny.cloud/docs-3x/reference/Configuration3x/Configuration3x@convert_urls/
+		 */
+		convertURLs : {type : Boolean, default : true},
+
+		labelLength : { type : [Number, String], default : 500 },
+
+		//FOR TOOLTIP POSITION
+		tipRule : { type : [Number, Boolean], default : false },
+
+	},
+
+	created () {
+		this.getCsrfToken();
+	},
+
+	data() {
+		return {
+			loading: false,
+			attachmentsArr : this.attachments,
+			editorValue : this.value,
+			options: {},
+			showMsg: false,
+		}
+	},
+
+	beforeMount() {
+		this.attachmentsArr=this.getAttachmentsData !=null ? this.getAttachmentsData :this.attachments;
+
+		const self = this;
+
+		this.options = {
+
+			max_height: self.height,
+			min_height: self.height,
+			menubar: (self.showMenubar) ? 'file edit view insert format tools table help' : false,
+			menu: {
+				view: {
+					title: 'View',
+					items: 'code | visualchars visualblocks | preview'
+				},
+			},
+			// this is to ensure that the text doesn't overlap at all in email clients
+			formats: {
+				fontsize: { inline: 'span', styles: { 'font-size': '%value', 'line-height': 'normal' } },
+			},
+			toolbar_sticky: self.toolbarSticky,
+			autosave_ask_before_unload: self.autoSaveAskBeforeUnload,
+			autosave_interval: self.autoSaveInterval,
+			autosave_prefix: 'tinymce-autosave-{path}{query}-{id}-',
+			autosave_restore_when_empty: self.autoSaveRestoreWhenEmpty,
+			autosave_retention: self.autoSaveRetention,
+			image_advtab: self.imageAdvancedTab,
+			forced_root_block_attrs: { "style": "margin: 0px;" }, // adds margin-0 to all p tags so it nullify the extra margin appending in mail
+			template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+			template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+			image_caption: self.imageCaption,
+			quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+			extended_valid_elements: 'div[onclick|style|class=toc|contenteditable]',
+			noneditable_noneditable_class: 'mceNonEditable',
+			toolbar_mode: self.toolbarMode,
+			contextmenu: false,
+			content_style: self.contentStyle,
+			draggable_modal: self.draggableModal,
+			elementpath: self.elementPath,
+			height: self.height,
+			resize: self.resize,
+			branding: self.branding,
+			browser_spellcheck: self.browserSpellcheck,
+			custom_undo_redo_levels: self.customUndoRedoLevels,
+			paste_data_images: self.pasteDataImages,
+			statusbar: self.statusbar,
+			auto_focus:self.autoFocus,
+			automatic_uploads: self.automaticUploads,
+			relative_urls: self.relativeUrls,
+			remove_script_host: self.removeScriptHost,
+			document_base_url: self.basePath(),
+			images_upload_url: self.basePath() + '/api/tiny-image-uploader?_token=' + document.querySelector('meta[name="csrf-token"]').getAttribute('content')+'&auth=false&api_key='+this.getApiKey,
+			convert_urls : self.convertURLs,
+			plugins: (self.page !== 'kb') ? plugins.filter(element => element !== 'media') : plugins,
+			text_patterns: self.textpattern_patterns,
+			setup: function(editor) {
+
+				editor.ui.registry.addButton("customtoc", {
+					text: "ToC",
+					onAction: () => editor.execCommand("customtoc"),
+				});
+
+				editor.addCommand("customtoc", function () {
+					const toc = generateTableOfContents(editor);
+					editor.insertContent(toc);
+				});
+
+				editor.on('Keyup', function () {
+					self.onChange(editor.getContent(),self.name)
+				});
+
+				editor.on('Change', function () {
+					self.onChange(editor.getContent(),self.name)
+				});
+
+				// Handle escape key to exit full screen tinymce
+				editor.on('keydown', function (e) {
+					if (e.key === 'Escape' && editor.plugins.fullscreen.isFullscreen()) {
+						// If the editor is maximized, minimize it
+						editor.execCommand('mceFullScreen');
+					}
+				});
+
+				// Add the margin-0 to all the p tags on pasting so it can nullify the extra margin appending in mail
+				editor.on('paste', () => {
+					setTimeout(() => {
+						// Access the content inside the editor
+						const paragraphs = editor.getBody().querySelectorAll('p');
+
+						// Add the desired styles to each <p> tag
+						paragraphs.forEach((p) => {
+							p.style.margin = '0px';
+						});
+					}, 0); // Allow the paste operation to complete before modifying
+				});
+
+				//Show popup for exiting full screen
+				editor.on('FullscreenStateChanged', (e) => {
+					if (e.state) {
+						// Maximized
+						self.showMsg = true;
+						setTimeout(()=>{
+							self.showMsg = false;
+						}, 3000)
+					} else {
+						// Minimized
+						self.showMsg = false;
+					}
+				});
+			},
+			toolbar: 'undo redo | bold italic underline strikethrough | customtoc | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media link codesample | ltr rtl'
+		}
+	},
+
+	watch: {
+		value: {
+			immediate: true,
+			handler (value, oldValue) {
+				this.editorValue = value;
+			}
+		},
+	},
+
+
+	computed: {
+		...mapGetters(['getAttachmentsData','getApiKey']),
+
+	},
+
+
+	methods: {
+
+		getCsrfToken() {
+
+			if(this.getApiKey) {
+
+				this.options.images_upload_url = this.basePath() + '/api/tiny-image-uploader?_token=' + document.querySelector('meta[name="csrf-token"]').getAttribute('content')+'&api_key='+this.getApiKey;
+
+			} else {
+
+				this.options.images_upload_url = this.basePath() + '/api/tiny-image-uploader?_token=' + document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+			}
+		},
+
+		removeAttachment(x){
+
+			this.attachmentsArr.splice(x,1);
+
+			this.getAttach(this.attachmentsArr);
+		},
+
+		async getAttachmentInfo(filesInfo) {
+			let files = JSON.stringify(filesInfo.files);
+			this.loading = true
+			try {
+				let response = await axios.get('file-manager/files-info', {
+					params: {names: files, disk: filesInfo.disk}
+				})
+				let data = response.data.data;
+				data.forEach((file) => {this.attachmentsArr.push(file)});
+				this.getAttach(this.attachmentsArr);
+
+			} catch (e) {
+				await this.$store.dispatch('setAlert', {
+					type: 'danger',
+					message: this.lang('file-manager-modal-error-message'),
+					component_name: 'faveo-form'
+				})
+			} finally {
+				this.loading = false;
+			}
+		},
+	},
+
+	components: {
+		editor: Editor,
+		'form-field-template': FormFieldTemplate,
+
+		// "file-manager-container" : FileManagerContainer
+	},
+}
+
+function generateTableOfContents(editor) {
+	const headings = editor.getBody().querySelectorAll("h1, h2, h3, h4, h5, h6");
+	if (!headings.length) return "";
+	let toc = "<div class='mce-toc' contenteditable='false'><h2 contenteditable='true'>Table of Contents</h2><ul>";
+	headings.forEach((heading, index) => {
+		const id = `toc_${index}`;
+		heading.setAttribute("id", id);
+		const headingText = heading.innerText.trim();
+		if (headingText) {
+			toc += `<li><a href="#${id}" class="toc_link">${headingText}</a></li>`;
+		}
+	});
+	toc += "</ul></div>";
+
+	return toc;
+}
+</script>
+
+<style scoped>
+#hidden-attach{
+	background-color: #f5f5f5;
+	border: 1px solid #dcdcdc;
+	font-weight: bold;
+	margin-top:9px;
+	overflow-y: hidden;
+	padding: 4px 4px 4px 8px;
+}
+
+.close-icon{
+	float:right;cursor: pointer;
+}
+
+.tox .tox-editor-header{
+	z-index: 1!important;
+}
+
+.tox .tox-dialog__body-nav-item:nth-child(4) {
+	display: none !important;
+}
+
+.tox .tox-dialog--width-lg{
+	max-width: 1000px!important;
+}
+</style>
